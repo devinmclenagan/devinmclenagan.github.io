3b16aa679cb72e703fed52dcab2176363ec9a05a
devinmclenagan <98351048+devinmclenagan@users.noreply.github.com>
2022-12-16 22:16:13 -0500

    final pages

---
+++ showitems.py
@@ -0,0 +1,13 @@
+def show_items(list, name):
+    x = []
+    for i in range(len(list)):
+        if name == list[i][0]:
+            x = list[i]
+    y = ""
+    for i in range(len(x)-1):
+        y = x[i]+", "+y
+    y += x[len(x)-1]
+    if y == "":
+        return "Item could not be found. Please enter a different item."
+    else:
+        return y
