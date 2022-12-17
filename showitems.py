9d655f2a974a1f87f91b016e46785cd4c17bcd2c
devinmclenagan <98351048+devinmclenagan@users.noreply.github.com>
2022-12-16 23:11:47 -0500

    update showitems.py

--- showitems.py
+++ showitems.py
@@ -5,7 +5,8 @@ def show_items(list, name):
             x = list[i]
     y = ""
     for i in range(len(x)-1):
-        y = x[i]+", "+y
+        if x[i] != name:
+            y = x[i]+", "+y
     y += x[len(x)-1]
     if y == "":
         return "Item could not be found. Please enter a different item."
